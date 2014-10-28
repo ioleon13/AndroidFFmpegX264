ceff92b52615636a87d09d2116cfa7ee8b86abc0
ioleon13 <liya.lhi@gmail.com>
2014-10-28 23:23:44 +0800

    Add functions
    Just add functions in TcpClient, to be continued.

--- src/com/livecamera/net/TcpClient.java
+++ src/com/livecamera/net/TcpClient.java
@@ -25,6 +25,7 @@ public class TcpClient {
     private int mServerPort1 = 8252;
     private int mServerPort2;
     private int mTimeoutConn = 10000;
+    private int mTimeoutRecv = 5000;
     private String mUrl;
     
     private byte[] mRecvBuff;
@@ -62,8 +63,20 @@ public class TcpClient {
         
         boolean isConnected = mURLClient.connect();
         if (isConnected) {
+        	mURLClient.start();
             mURLClient.sendCmd1();
         }
+    }
+    
+    
+    /**
+     * send encoded data to server
+     * @param data
+     */
+    public void sendStreamData(byte[] data) {
+    	if (mStreamClient != null) {
+			mStreamClient.sendCmd5(data);
+		}
     }
     
     
@@ -112,7 +125,11 @@ public class TcpClient {
             public void handleMessage(Message msg) {
                 switch (msg.what) {
                 case MessageTypes.PORT2_RECEIVED:
-                    
+                	mServerPort2 = msg.arg1;
+                	mUrl = (String)msg.obj;
+                	
+                    //stop UrlClient and start StreamClient to send Cmd3
+                	startStreamClient();
                     break;
                     
                 case MessageTypes.STREAMING_ALLOWED:
@@ -132,6 +149,26 @@ public class TcpClient {
     }
     
     
+    private void startStreamClient() {
+    	//first: stop UrlClient
+    	if (mURLClient != null) {
+			mURLClient.stop();
+		}
+    	
+    	//second: StreamClient connect
+    	if (mStreamClient != null) {
+    		boolean isConnected = mStreamClient.connect();
+    		
+    		//third: start listen and send Cmd3
+    		if (isConnected) {
+				mStreamClient.start();
+				mStreamClient.sendCmd3();
+			}
+		}
+    	
+    }
+    
+    
     private void shutdown() {
         try {
             mSocket.close();
@@ -146,6 +183,7 @@ public class TcpClient {
     public class UrlClient implements Runnable {
         private boolean mIsStop = false;
         private Handler mHandler = null;
+        private long mCmd1SendTime = 0l;
         
         public void setHandler(Handler handler) {
             this.mHandler = handler;
@@ -190,6 +228,7 @@ public class TcpClient {
             }
             
             Log.i(TAG, "Successed to send Cmd1");
+            mCmd1SendTime = System.currentTimeMillis();
         }
         
         
@@ -202,7 +241,12 @@ public class TcpClient {
                 if (bytes > 0) {
                     mIn.read(mRecvBuff, 0, 1024);
                     //String data = new String(mRecvBuff, 0, 1024);
-                    handleRecvData(mRecvBuff);
+                    int ret = handleRecvData(mRecvBuff);
+                    
+                    if (ret != 0) {
+						Log.e(TAG, "Failed to receive Cmd2");
+						shutdown();
+					}
                 }
             } catch (IOException e) {
                 Log.e(TAG, "Failed to receive Cmd2 from server");
@@ -222,37 +266,65 @@ public class TcpClient {
             int size = data.length;
             if (size == 0) {
                 Log.e(TAG, "Receiving data size is 0");
-                return 2;
+                return 1;
             }
             ByteBuffer buff = ByteBuffer.wrap(data);
             buff.order(ByteOrder.LITTLE_ENDIAN);
             if (data[0] != 0x82) {
-                return 2;
+                return 1;
             }
             
             short cmdNum = buff.getShort(1);
             if (cmdNum != 2) {
-                return 2;
+                return 1;
             }
             
             int cmdLen = buff.getInt(3);
             int urlLen = buff.getInt(7);
             
             if (urlLen == 0 || (11+urlLen+4) != size) {
-                return 2;
+                return 1;
             }
             
             byte[] url = new byte[urlLen];
             buff.get(url, 11, urlLen);
-            String strUrl = new String(url, 0, urlLen);
-            mServerPort2 = buff.getInt(11 + urlLen);
+            String parsedUrl = new String(url, 0, urlLen);
+            int port = buff.getInt(11 + urlLen);
+            Log.i(TAG, "Success to receive Cmd2 <" + parsedUrl + " : " + port + ">");
+            
+            //send message
+            handleRecvMessage(cmdNum, parsedUrl, port);
             
             return 0;
+        }
+        
+        
+        private void handleRecvMessage(int cmdNum, String url, int port) {
+        	Message message = new Message();
+        	switch (cmdNum) {
+			case 2:
+				Log.i(TAG, "HandleMessage: Cmd2 received, send to TcpClient to handle");
+				message.what = MessageTypes.PORT2_RECEIVED;
+				message.obj = url;
+				message.arg1 = port;
+				if (mHandler != null) {
+					mHandler.sendMessage(message);
+				}
+				break;
+
+			default:
+				break;
+			}
         }
 
         @Override
         public void run() {
             while (!mIsStop) {
+            	if (System.currentTimeMillis() - mCmd1SendTime > mTimeoutRecv) {
+            		Log.e(TAG, "Failed to Receive Cmd2: Timeout");
+					break;
+				}
+            	
                 read();
                 try {
                     Thread.sleep(100);
@@ -264,6 +336,11 @@ public class TcpClient {
             shutdown();
         }
         
+        public synchronized void start() {
+        	Thread listenThread = new Thread(this);
+        	listenThread.start();
+        }
+        
         public synchronized void stop() {
             mIsStop = true;
         }
@@ -273,6 +350,7 @@ public class TcpClient {
     public class StreamingClient implements Runnable {
         private boolean mIsStop = false;
         private Handler mHandler = null;
+        private long mCmd3SendTime = 0l;
         
         public void setHandler(Handler handler) {
             this.mHandler = handler;
@@ -284,9 +362,168 @@ public class TcpClient {
 
         @Override
         public void run() {
-            // TODO Auto-generated method stub
+        	while (!mIsStop) {
+            	if (System.currentTimeMillis() - mCmd3SendTime > mTimeoutRecv) {
+            		Log.e(TAG, "Failed to Receive Cmd4: Timeout");
+					break;
+				}
+            	
+                read();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             
+            shutdown();
         }
         
+        
+        /**
+         * read the feedback from server
+         */
+        private void read() {
+        	try {
+                int bytes = mIn.available();
+                if (bytes > 0) {
+                    mIn.read(mRecvBuff, 0, 1024);
+                    //String data = new String(mRecvBuff, 0, 1024);
+                    int ret = handleRecvData(mRecvBuff);
+                    
+                    if (ret != 0) {
+						Log.e(TAG, "Failed to receive Cmd");
+						shutdown();
+					}
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to receive Cmd from server");
+                e.printStackTrace();
+            }
+        }
+        
+        
+        private int handleRecvData(byte[] data) {
+        	int ret = 0;
+        	
+        	int size = data.length;
+            if (size == 0) {
+                Log.e(TAG, "Receiving data size is 0");
+                return 1;
+            }
+            ByteBuffer buff = ByteBuffer.wrap(data);
+            buff.order(ByteOrder.LITTLE_ENDIAN);
+            if (data[0] != 0x82) {
+                return 1;
+            }
+            
+            short cmdNum = buff.getShort(1);
+            
+            switch (cmdNum) {
+			case 4:
+				ret = handleRecvCmd4(buff);
+				break;
+				
+			case 6:
+				ret = handleRecvCmd6(buff);
+				break;
+
+			default:
+				Log.e(TAG, "Unknown Cmd type");
+				ret = 1;
+				break;
+			}
+            
+        	return ret;
+        }
+        
+        
+        /**
+         * Is allowed to send stream data
+         * @param buff
+         * Cmd4:
+         * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         * |Prefix=0x82(1Byte) |  Num=4(2Byte) |  Len(4Byte)   |  Accessable=0(1Byte)  |
+         * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         * @return 0-success 1-received an error data or unknown cmd
+         */
+        private int handleRecvCmd4(ByteBuffer buff) {
+        	int ret = 0;
+        	int cmdLen = buff.getInt(3);
+        	
+        	return ret;
+        }
+        
+        
+        /**
+         * Heartbeat info
+         * @param buff
+         * Cmd6:
+         * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         * |Prefix=0x82(1Byte) |  Num=6(2Byte) |  Len(4Byte)   |         String        |
+         * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         * @return 0-success 1-received an error data or unknown cmd
+         */
+        private int handleRecvCmd6(ByteBuffer buff) {
+        	int ret = 0;
+        	return ret;
+        }
+        
+        
+        /**
+         * Cmd3:
+         * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         * |Prefix=0x82(1Byte) |  Num=3(2Byte) |  Len(4Byte)   |  ClientType=0(1Byte)  |
+         * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         */
+        public void sendCmd3() {
+        	byte[] prefix = {(byte) 0x82};
+            byte[] cmdNum = new byte[2];
+            ByteBuffer buff = ByteBuffer.wrap(cmdNum);
+            buff.order(ByteOrder.LITTLE_ENDIAN);
+            buff.putShort((short) 3);
+            byte[] len = new byte[4];
+            buff = ByteBuffer.wrap(len);
+            buff.order(ByteOrder.LITTLE_ENDIAN);
+            buff.putInt((int) 1);
+            byte[] clientType = {(byte)0x00};
+            
+            //send
+            try {
+                mOut.write(prefix, 0, prefix.length);
+                mOut.write(cmdNum, 0, cmdNum.length);
+                mOut.write(len, 0, len.length);
+                mOut.write(clientType, 0, clientType.length);
+                mOut.flush();
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to send cmd3, IOException caught");
+                e.printStackTrace();
+            }
+            
+            Log.i(TAG, "Successed to send Cmd3");
+            mCmd3SendTime = System.currentTimeMillis();
+        }
+        
+        
+        /**
+         * send H.264 payload
+         * @param data
+         * Cmd5:
+         * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         * |Prefix=0x82(1Byte) |  Num=5(2Byte) |  Len(4Byte)   |  Timestamp(4Byte) | PayloadLen(4Byte) |  H.264 payload  |
+         * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+         */
+        public void sendCmd5(byte[] data) {
+        	
+        }
+        
+        public synchronized void start() {
+        	Thread listenThread = new Thread(this);
+        	listenThread.start();
+        }
+        
+        public synchronized void stop() {
+            mIsStop = true;
+        }
     }
 }
